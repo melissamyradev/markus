ba97dafba5115a128e4831aabf754ffda5c2d30d
Melissa <melissamyradev@gmail.com>
2022-03-14 21:44:44 +0800

    Altered homepage hero heading

--- js/script.js
+++ js/script.js
@@ -67,7 +67,7 @@
       $('#home-header nav').removeClass('nav-sticky');
     }
 
-    if (scroll > 150) { 
+    if (scroll > 200) { 
       $('#home-header h1').addClass('fade-out');
     } else {
       $('#home-header h1').removeClass('fade-out');
