8fdc51b2e145704f2ed639c941fee26b09f125bb
Melissa <melissamyradev@gmail.com>
2022-03-14 20:32:24 +0800

    Turn off slide autoplay

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@
     },1000);
 
     var splide = new Splide( '.splide', {
-      autoplay    : true,
+      autoplay    : false,
       rewind      : true,
       pauseOnHover: true,
       pauseOnFocus: false,
@@ -287,18 +287,6 @@ gsap.utils.toArray(".parallax-containers").forEach((section, i) => {
   }
 });
 
-  
-
-  // usage:
-batch("section p", {
-  interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
-  batchMax: 3,   // maximum batch size (targets)
-  onEnter: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
-  onLeave: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true}),
-  onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.15, overwrite: true}),
-  onLeaveBack: batch => gsap.set(batch, {autoAlpha: 0, overwrite: true})
-  // you can also define things like start, end, etc.
-});
 
 /* ========================================================================= */
   /*	Slideshow
